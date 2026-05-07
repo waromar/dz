5b9a48d2b0db9727e27447005090c7d51847f65f
Artemy Yugai <donutandjem@gmail.com>
2026-05-07 20:08:31 +0500

    пофиксили константы, для соблюдения условий задачи

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,12 +1,7 @@
 package main
-import "fmt"
-const (
-	USDinEUR float64 = 0.8497
-	USDinRUB float64 = 75.22
-)
+
 func main() {
-	//Рассчитать EUR в RUB на основании первых двух
-	var EUR float64 = 100
-	EURinRUB := EUR / USDinEUR * USDinRUB
-	fmt.Println(EURinRUB)
+	const USDinEUR float64 = 0.8497
+	const USDinRUB float64 = 75.22
+	const EURinRUB float64 = 100 / USDinEUR * USDinRUB
 }
