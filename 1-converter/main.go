663f98ccd0cf503c49878895bf9d08265f1316ef
Artemy Yugai <donutandjem@gmail.com>
2026-05-07 20:13:08 +0500

    fix const EURinRUB

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,5 +3,5 @@ package main
 func main() {
 	const USDinEUR float64 = 0.8497
 	const USDinRUB float64 = 75.22
-	const EURinRUB float64 = 100 / USDinEUR * USDinRUB
+	const EURinRUB float64 = USDinEUR * USDinRUB
 }
