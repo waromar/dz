c88227beb00e5171a0fafb72ae935e087605ef75
Artemy Yugai <donutandjem@gmail.com>
2026-05-07 20:37:25 +0500

    Add func scanNumber and func calculate

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,14 @@
 package main
-
+import "fmt"
 func main() {
 	const USDinEUR float64 = 0.8497
 	const USDinRUB float64 = 75.22
 	const EURinRUB float64 = USDinEUR * USDinRUB
+
+}
+func scanNumber(number float64) {
+	fmt.Scan(&number)
+}
+func calculate(num float64, currency1, currency2 string){
+
 }
