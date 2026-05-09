ed279b80aab8077d71f0b23fb0c093471aa9714a
Artemy Yugai <donutandjem@gmail.com>
2026-05-09 22:35:05 +0500

    FULL fix and add func calculate

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 )
-
+const RUBinEUR float64 = USDinEUR / USDinRUB
+const EURinUSD float64 = 1 / USDinEUR
 const USDinEUR float64 = 0.8497
 const USDinRUB float64 = 75.22
 const EURinRUB float64 = USDinRUB / USDinEUR
-
+const RUBinUSD float64 = 1 / USDinRUB
 func main() {
 	// вызов функции ввода валюты
 	currency, err := inputCurrency()
@@ -28,7 +29,8 @@ func main() {
 		goalCurrency,_ = inputGoalCurrency(currency)
 	}
 	// вызов функции для расчета
-	fmt.Println(currency,value,goalCurrency)
+	sum := calculate(value, currency, goalCurrency)
+	fmt.Println("Вывод:", sum)
 }
 func inputValue() (float64, error) {
 	fmt.Println("Введите количество валюты:")
@@ -59,15 +61,52 @@ func inputGoalCurrency(currency string) (string, error) {
 	switch currency {
 	case "RUB":
 		fmt.Println("Введите целевую валюту: EUR, USD")
+		_, err := fmt.Scan(&goalCurrency)
+		if err != nil {
+			return "", fmt.Errorf("Ошибка валюты")
+		}
+		if goalCurrency != "EUR" && goalCurrency != "USD" {
+			return "", fmt.Errorf("Введена бурда")
+		 }
+
 	case "EUR":
 		fmt.Println("Введите целевую валюту: RUB, USD")
+		_, err := fmt.Scan(&goalCurrency)
+		if err != nil {
+			return "", fmt.Errorf("Ошибка валюты")
+		}
+		if goalCurrency != "RUB" && goalCurrency != "USD" {
+			return "", fmt.Errorf("Введена бурда")
+		}
 	case "USD":
 		fmt.Println("Введите целевую валюту: RUB, EUR")
+		_, err := fmt.Scan(&goalCurrency)
+		if err != nil {
+			return "", fmt.Errorf("Ошибка валюты")
+			
+		}
+		if goalCurrency != "RUB" && goalCurrency != "EUR" {
+			return "", fmt.Errorf("Введена бурда")
+		}
 	}
-	_, err := fmt.Scan(&goalCurrency)
-	if err != nil {
-		return "", fmt.Errorf("Ошибка валюты")
-	}
-	return goalCurrency, nil
 
+
+	return goalCurrency, nil
 }
+func calculate(value float64, currency string, goalCurrency string) float64 {
+if currency == "RUB" && goalCurrency == "EUR"	{
+ return value * RUBinEUR
+} else if currency == "USD" && goalCurrency == "EUR" {
+ return value * USDinEUR
+} else if currency == "RUB" && goalCurrency == "USD" {
+ return value * RUBinUSD
+} else if currency == "USD" && goalCurrency == "RUB" {
+ return value * USDinRUB 
+} else if currency == "EUR" && goalCurrency == "RUB" {
+ return value * EURinRUB
+} else if currency == "EUR" && goalCurrency == "USD" {
+ return value * EURinUSD
+} 
+return value
+}
+
