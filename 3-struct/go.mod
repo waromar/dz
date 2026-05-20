6498338efb451c80d0d95fc498e2693a7208dd4e
Artemy Yugai <donutandjem@gmail.com>
2026-05-20 07:30:55 +0500

    123

---
+++ 3-struct/go.mod
@@ -0,0 +1,3 @@
+module demo/3-struct
+
+
--- 3-struct/main.go
+++ 3-struct/main.go
@@ -4,46 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"time"
+	"demo/3-struct/bins"
 )
 
-type Bin struct {
-	ID        string
-	Private   bool
-	CreatedAt time.Time
-	Name      string
-}
-
-type BinList struct {
-	Bins []Bin
-}
 
 func main() {
 
 }
 
-func initBin(id string, private bool, name string) (*Bin, error) {
-	if id == "" {
-		return &Bin{}, errors.New("Ошибка ID")
-	}
-	if name == "" {
-		return &Bin{}, errors.New("Ошибка name")
-	}
-	if private == true {
-		fmt.Println("Bin локальный")
-	} else if private == false {
-		fmt.Println("Bin публичный")
-	}
-
-	return &Bin{
-		ID:        id,
-		Private:   private,
-		Name:      name,
-		CreatedAt: time.Now(),
-	}, nil
-}
-
-func InitBins(bins []Bin) *BinList {
-	return &BinList{
-		Bins: bins,
-	}
-}
